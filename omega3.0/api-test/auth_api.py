0e0ea524f41a6d94754430cbb6836692bc1ece3b
fangzhihao <154396937@qq.com>
2016-09-05 20:28:04 +0800

    auto api

--- omega3.0/api-test/auth_api.py
+++ omega3.0/api-test/auth_api.py
@@ -1,8 +1,11 @@
 import requests
 import json
+import configparser
 from  Logger import Logger
-url = "http://192.168.1.155:9999"
 
+cf = configparser.ConfigParser()
+cf.read("test.conf")
+url = cf.get("all", "url")
 
 class authapi(object):
 
@@ -14,45 +17,44 @@ class authapi(object):
     def login(self, email, password, expectcode):
         payload = {"email": email ,"password": password}
         re = requests.post(url + "/v1/login" ,data=json.dumps(payload))
+        apicase='login'
         if expectcode ==200 and re.status_code == expectcode :
-            Logger.log_normal("test valid login ")
+            Logger.log_normal("valid  "+apicase)
             if len(re.json()["data"])==32 and int(re.json()["code"])==0 :
-                Logger.log_high("valid login test passed")
+                Logger.log_high("valid "+apicase+" passed")
                 return re.json()['data']
             else:
-                Logger.log_fail("valid login test failed")
+                Logger.log_fail("valid "+apicase+" failed")
         elif expectcode ==400 and re.status_code == expectcode:
-            Logger.log_normal("test invalid login ")
+            Logger.log_normal("invalid "+apicase )
             if int(re.json()["code"])==12007 :
-                Logger.log_high("invalid login test passed")
+                Logger.log_high("invalid "+apicase+" passed")
             else:
-                Logger.log_fail("invalid login test failed return code is "+int(re.json()["code"])+"but it must be 12007")
-        elif(re.status_code!=200 and re.status_code!=400):
-            Logger.log_fail("uncatched error" + str(re.status_code))
+                Logger.log_fail("invalid "+apicase+" failed return code is "+int(re.json()["code"])+"but it must be 12007")
         else:
-            Logger.log_fail("uncatched error" + str(re.status_code))
+            Logger.log_fail(apicase+" uncatched error " + str(re.status_code))
+
+
 
 
     #2退出登录
     def logout(self,token,expectcode):
         re = requests.post(url + "/v1/logout" ,headers={'Authorization': token})
+        apicase = 'logout'
         if expectcode ==200 and re.status_code == expectcode :
-            Logger.log_normal("test valid login ")
-            if len(re.json()["data"])==32 and int(re.json()["code"])==0 :
-                Logger.log_high("valid login test passed")
-                return re.json()['data']
+            Logger.log_normal("valid  " + apicase)
+            if re.json()["data"]=='success' and int(re.json()["code"])==0 :
+                Logger.log_high("valid " + apicase + " passed")
             else:
-                Logger.log_fail("valid login test failed")
-        elif expectcode ==400 and re.status_code == expectcode:
-            Logger.log_normal("test invalid login ")
-            if int(re.json()["code"])==12007 :
-                Logger.log_high("invalid login test passed")
+                Logger.log_fail("valid " + apicase + " failed")
+        elif expectcode ==401 and re.status_code == expectcode:
+            Logger.log_normal("invalid " + apicase)
+            if int(re.json()["code"])==1 :
+                Logger.log_high("invalid " + apicase + " passed")
             else:
-                Logger.log_fail("invalid login test failed return code is "+int(re.json()["code"])+"but it must be 12007")
-        elif(re.status_code!=200 and re.status_code!=400):
-            Logger.log_fail("uncatched error" + str(re.status_code))
+                Logger.log_fail("invalid "+apicase+" test failed return code is "+int(re.json()["code"])+"but it must be 1")
         else:
-            Logger.log_fail("uncatched error" + str(re.status_code))
+            Logger.log_fail(apicase+" uncatched error " + str(re.status_code))
 
     #3修改密码
     def change_passwd(self,token):
@@ -63,20 +65,43 @@ class authapi(object):
             return re.status_code ,re.json()
 
     # 4获取当前用户信息
-    def getuserinfo(self, token):
+    def getuserinfo(self, token,expectcode):
         re = requests.get(url + "/v1/aboutme", headers={'Authorization': token})
-        if re.status_code != 200:
-            return re.status_code, re.json()
-        elif re.json()['code'] == 0:
-            return re.status_code, re.json()
+        apicase = 'get user info'
+        if expectcode ==200 and re.status_code == expectcode :
+            Logger.log_normal("valid  " + apicase)
+            if re.json()["data"]['email'] != None and int(re.json()["code"])==0 :
+                Logger.log_high("valid " + apicase + " passed")
+            else:
+                Logger.log_fail("valid " + apicase + " failed")
+        elif expectcode ==401 and re.status_code == expectcode:
+            Logger.log_normal("invalid " + apicase)
+            if int(re.json()["code"])==1 :
+                Logger.log_high("invalid " + apicase + " passed")
+            else:
+                Logger.log_fail("invalid "+apicase+" test failed return code is "+int(re.json()["code"])+"but it must be 1")
+        else:
+            Logger.log_fail(apicase+" uncatched error " + str(re.status_code))
+
 
     #5获取所有用户
-    def get_all_user_info(self,token):
+    def get_all_user_info(self,token,expectcode):
         re = requests.get(url + "/v1/accounts" ,headers={'Authorization': token})
-        if re.status_code != 200:
-            return re.status_code ,re.json()
-        elif re.json()['code'] == 0:
-            return re.status_code ,re.json()
+        apicase = 'get all user info'
+        if expectcode ==200 and re.status_code == expectcode :
+            Logger.log_normal("valid  " + apicase)
+            if  int(re.json()["code"])==0 :
+                Logger.log_high("valid " + apicase + " passed")
+            else:
+                Logger.log_fail("valid " + apicase + " failed")
+        elif expectcode ==401 and re.status_code == expectcode:
+            Logger.log_normal("invalid " + apicase)
+            if int(re.json()["code"])==1 :
+                Logger.log_high("invalid " + apicase + " passed")
+            else:
+                Logger.log_fail("invalid " + apicase + " test failed return code is " + int(re.json()["code"]) + "but it must be 1")
+        else:
+            Logger.log_fail(apicase + "uncatched error " + str(re.status_code))
 
     #6创建用户 need docs
 
@@ -84,9 +109,26 @@ class authapi(object):
 
 
     #7根据用户id获取用户信息
-    def get_groupinfo_byuserid(self,token,id):
+    def get_groupinfo_byuserid(self,token,id,expectcode):
         re = requests.get(url + "/v1/accounts/"+str(id) ,headers={'Authorization': token})
-        if re.status_code != 200:
-            return re.status_code ,re.json()
-        elif re.json()['code'] == 0:
-            return re.status_code ,re.json()
+        apicase = 'get group info  by userid'
+        if expectcode ==200 and re.status_code == expectcode :
+            Logger.log_normal("valid  " + apicase)
+            if  int(re.json()["code"])==0 :
+                Logger.log_high("valid " + apicase + " passed")
+            else:
+                Logger.log_fail("valid " + apicase + " failed")
+        elif expectcode ==401 and re.status_code == expectcode:
+            Logger.log_normal("invalid " + apicase)
+            if int(re.json()["code"])==1 :
+                Logger.log_high("invalid " + apicase + " passed")
+            else:
+                Logger.log_fail("invalid " + apicase + " test failed return code is " + int(re.json()["code"]) + "but it must be 1")
+        elif expectcode ==404 and re.status_code == expectcode:
+            Logger.log_normal("invalid " + apicase)
+            if int(re.json()["code"])==1 :
+                Logger.log_high("invalid " + apicase + " passed")
+            else:
+                Logger.log_fail("ttest get group info by userid failed return code is "+int(re.json()["code"])+"but it must be 1")
+        else:
+            Logger.log_fail(apicase + "uncatched error " + str(re.status_code))
