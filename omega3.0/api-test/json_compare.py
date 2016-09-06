f74af9810ec94ceaf0581683649060126b8d7910
CCissie <cchen0628@qq.com>
2016-09-06 18:22:25 +0800

    modify json compare function, modify metrics test, and add metrics json file

--- omega3.0/api-test/json_compare.py
+++ omega3.0/api-test/json_compare.py
@@ -1,19 +1,72 @@
 import json
 from  Logger import Logger
 
+# def json_compare(re_data,jsonfile,api_name):
+#     j = json.load(open(jsonfile))
+#     fail_flag = 0
+#     for i in j:
+#         if i["api"] == api_name:
+#             data = i["response"]["data"]
+#             for key in data:
+#                 if isinstance(key,dict):
+#                     data2 = key
+#                     for key2 in data2:
+#                         try:
+#                             re_data[0][key]
+#                         except:
+#                             msg_fail = key2 + " isn't in response of " + i["api"]
+#                             Logger.log_fail(msg_fail)
+#                             fail_flag = 1
+#                     break
+#                 else:
+#                     try:
+#                         re_data[key]
+#                     except:
+#                         msg_fail = key + " isn't in response of " + i["api"]
+#                         Logger.log_fail(msg_fail)
+#                         fail_flag = 1
+#
+#     if fail_flag == 0:
+#         msg_pass = i["api"] + " response json is correct."
+#         Logger.log_pass(msg_pass)
+
 def json_compare(re_data,jsonfile,api_name):
     j = json.load(open(jsonfile))
     fail_flag = 0
     for i in j:
         if i["api"] == api_name:
             data = i["response"]["data"]
-            for key in data:
-                if re_data.get(key,None) == None:
-                    msg_fail = key + "isn't in response of "+ i["api"]
-                    Logger.log_fail(msg_fail)
-                    fail_flag = 1
+            #如果data是list
+            if isinstance(data,list):
+                data2 = data[0]
+                re_data2 = re_data[0]
+                for key in data2:
+                    try:
+                        get_key = re_data2[key]
+                    except:
+                        msg_fail = key + " isn't in response of " + i["api"]
+                        Logger.log_fail(msg_fail)
+                        fail_flag = 1
+                    break
+            #如果data是dict
+            else:
+                for key in data:
+                    try:
+                        get_key = re_data[key]
+                    except:
+                        msg_fail = key + " isn't in response of " + i["api"]
+                        Logger.log_fail(msg_fail)
+                        fail_flag = 1
+
     if fail_flag == 0:
-        msg_pass = api_name + " response json is correct."
+        msg_pass = i["api"] + " response json is correct."
         Logger.log_pass(msg_pass)
 
-#if __name__ == '__main__':
+
+if __name__ == '__main__':
+    j = json.load(open("metrics.json"))
+    for i in j:
+        if i["api"] == "GET /v1/nodes/:node_ip/instances":
+            re_data = i["response"]["data"]
+
+    json_compare2(re_data, "metrics.json", "GET /v1/nodes/:node_ip/instances")
--- omega3.0/api-test/metrics.py
+++ omega3.0/api-test/metrics.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import configparser
-from  auth_api import authapi
+# from  auth_api import authapi
 from  Logger import Logger
 from json_compare import json_compare
 
@@ -9,31 +9,37 @@ cf = configparser.ConfigParser()
 cf.read("test.conf")
 url = cf.get("all", "url")
 
+
 def gettoken( email , password):
     payload = {"email": email , "password": password}
-    re = requests.post(url + "/v1/login" , data=json.dumps(payload))
-    return re.json()['data']
+    try:
+        re = requests.post(url + "/v1/login" , data=json.dumps(payload))
+        return re.json()['data']
+    except:
+        Logger.log_fail("Can't get token")
+        return None
+
+TOKEN = gettoken('admin@dataman-inc.com', 'Dataman1234')
 
 class metrics(object):
 
     def __init__(self):
-        l = authapi()
-        self.token = gettoken('admin@dataman-inc.com', 'Dataman1234')
+        pass
 
     #获取集群主机列表 /v1/nodes
     def get_nodes(self):
-        re = requests.get(url + "/v1/nodes",headers={'Authorization': self.token})
-        return re.status_code ,re.json()
+        re = requests.get(url + "/v1/nodes",headers={'Authorization': TOKEN})
+        return re.status_code ,re.json(), re.text
 
     #获取指定主机的信息 /v1/nodes/:node_ip/info
     def get_nodes_info(self,node_ip):
-        re = requests.get(url + "/v1/nodes/" + node_ip + "/info",headers={'Authorization': self.token})
-        return re.status_code, re.json()
+        re = requests.get(url + "/v1/nodes/" + node_ip + "/info",headers={'Authorization': TOKEN})
+        return re.status_code, re.json(), re.text
 
     #获取指定主机的容器列表 /v1/nodes/:node_ip/instances
     def get_nodes_instances(self,node_ip):
-        re = requests.get(url + "/v1/nodes/" + node_ip + "/instances",headers={'Authorization': self.token})
-        return re.status_code, re.json()
+        re = requests.get(url + "/v1/nodes/" + node_ip + "/instances",headers={'Authorization': TOKEN})
+        return re.status_code, re.json(), re.text
 
 
 def assert_status_code(re,assert_code):
@@ -43,61 +49,77 @@ def assert_status_code(re,assert_code):
         Logger.log_pass("Response http code is " + str(assert_code) + " as expected.")
 
 if __name__ == '__main__':
-    #GET /v1/nodes/ 测试===获取集群主机列表 /v1/nodes==============================================
     m = metrics()
+    #GET /v1/nodes/ 测试===获取集群主机列表 /v1/nodes==============================================
     re = m.get_nodes()
 
     #测试GET /v1/nodes/返回的http_code
-    Logger.log_info("Test get nodes response http code")
+    Logger.log_info("1. Test get nodes response http code")
     assert_status_code(re, 200)
 
     #测试GET /v1/nodes/返回的data中的元素
-    Logger.log_info("Test get nodes reponse json")
-    if re[1]["data"].get('masters',None) == None:
-        Logger.log_fail("Get /v1/node data don't include masters")
-    elif re[1]["data"].get('slaves',None) == None:
-        Logger.log_fail("Get /v1/node data don't include slaves")
-    elif re[1]["data"]["masters"][0].get('id',None) == None:
-        Logger.log_fail("Get /v1/node data don't include master-id")
-    elif re[1]["data"]["masters"][0].get('ip',None) == None:
-        Logger.log_fail("Get /v1/node data don't include master-ip")
-    else:
-        Logger.log_pass("Get /v1/node reponse json is correct")
+    Logger.log_info("2. Test get nodes reponse json")
+    json_compare(re[1]["data"], "metrics.json", "GET /v1/nodes/")
+
+    print(re[2])
 
     # GET /v1/nodes/:node_ip/info 测试 - 获取master ===获取指定主机的信息 /v1/nodes/:node_ip/info==================================
     # 测试 ip 为 master 时 GET /v1/nodes/:node_ip/info返回的http_code
-    Logger.log_info("Test get nodes info response http code with master ip")
+    Logger.log_info("3. Test get nodes info response http code with master ip")
     master_ip = re[1]["data"]["masters"][0].get('ip', None)
+
     re2 = m.get_nodes_info(master_ip)
     assert_status_code(re2, 200)
 
-    # 测试 GET /v1/nodes/:node_ip/info 中的元素
-    Logger.log_info("Test get node info reponse json")
-    json_compare(re2[1]["data"], "api_response.json", "GET /v1/nodes/:node_ip/info")
+    # 测试 GET /v1/nodes/:node_ip/info 中的元素 - master_ip
+    Logger.log_info("4. Test get node info reponse json with master ip")
+    json_compare(re2[1]["data"], "metrics.json", "GET /v1/nodes/:node_ip/info")
+
+    print(re2[2])
 
     # 测试 ip 为 slave 时 GET /v1/nodes/:node_ip/info返回的http_code
-    Logger.log_info("Test get nodes info response http code with slave ip")
+    Logger.log_info("5. Test get nodes info response http code with slave ip")
     slave_ip = re[1]["data"]["slaves"][0].get('ip', None)
+
     re2 = m.get_nodes_info(master_ip)
     assert_status_code(re2, 200)
 
+    # 测试 GET /v1/nodes/:node_ip/info 中的元素 - slave_ip
+    Logger.log_info("6. Test get node info reponse json with slave ip")
+    json_compare(re2[1]["data"], "metrics.json", "GET /v1/nodes/:node_ip/info")
+
+    print(re2[2])
+
     #测试 ip 不存在时 GET /v1/nodes/:node_ip/info返回的http_code
-    Logger.log_info("Test get nodes info response http code with invalid ip")
+    Logger.log_info("7. Test get nodes info response http code with invalid ip")
     re2 = m.get_nodes_info("192.168.1.1")
     assert_status_code(re2, 404)
 
+
     # GET /v1/nodes/:node_ip/instances===获取指定主机的容器列表 /v1/nodes/:node_ip/instances==================================
     # 测试 ip 为 master 时  GET /v1/nodes/:node_ip/instances 返回的http_code
-    Logger.log_info("Test get nodes instances response http code with master ip")
+    Logger.log_info("8. Test get nodes instances response http code with master ip")
     re3 = m.get_nodes_instances(master_ip)
     assert_status_code(re3, 200)
 
+    # # 测试 ip 为 master 时  GET /v1/nodes/:node_ip/instances 返回的response json
+    Logger.log_info("9. Test get nodes instances response json with master ip")
+    json_compare(re3[1]["data"], "metrics.json", "GET /v1/nodes/:node_ip/instances")
+
+    print(re3[2])
+
     # 测试 ip 为 slave 时  GET /v1/nodes/:node_ip/instances 返回的http_code
-    Logger.log_info("Test get nodes instances response http code with slave ip")
+    Logger.log_info("10. Test get nodes instances response json with slave ip")
     re3 = m.get_nodes_instances(slave_ip)
     assert_status_code(re3, 200)
 
+    # 测试 ip 为 slave 时  GET /v1/nodes/:node_ip/instances 返回的response json
+    Logger.log_info("11. Test get nodes instances response json with slave ip")
+    json_compare(re3[1]["data"], "metrics.json", "GET /v1/nodes/:node_ip/instances")
+
+    print(re3[2])
+
     # 测试 ip 不存在时 GET /v1/nodes/:node_ip/instances 返回的http_code
-    Logger.log_info("Test get nodes instances response http code with invalid ip")
+    Logger.log_info("12. Test get nodes instances response http code with invalid ip")
     re3 = m.get_nodes_instances("192.168.1.1")
     assert_status_code(re3, 404)
